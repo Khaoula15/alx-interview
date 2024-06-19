8b52c0cea7ff41ff5ffc62a0f014b43af03f9f97
khaoula15 <khaoulasmp@gmail.com>
2024-06-19 17:59:13 +0000

    updated

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,38 +1,23 @@
 #!/usr/bin/python3
 """
-Defines function that determines the fewest number of coins to make change
+Main file for testing
 """
 
 
-def makeChange(coins, total):
+def makeChange(coins, amount):
     """
-    Determines the fewest number of coins needed to meet a given total
-
-    parameters:
-        coins [list or positive ints]:
-            the values of the coins in your possession
-            you can assume you have an infinite number of coins of all values
-        total [int]:
-            total amount of change to make
-            if total is 0 or less, return 0
-
-    returns:
-        the fewest number of coins to make the change
-        or -1 if the total change cannot be made with the given coins
+    How many of this type of coin can I get with my money? Okay,
+        I'll take that many. Now, how much money do I have left?
+        And how many coins do I have in my pocket?
     """
-    if total <= 0:
+    if amount < 1:
         return 0
-    if len(coins) is 0:
-        return -1
-    coins = sorted(coins)
-    dynamic = [float('inf')] * (total + 1)
-    dynamic[0] = 0
-    for i in range(total + 1):
-        for coin in coins:
-            if coin > i:
-                break
-            if dynamic[i - coin] != -1:
-                dynamic[i] = min(dynamic[i - coin] + 1, dynamic[i])
-    if dynamic[total] == float('inf'):
-        return -1
-    return dynamic[total]
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        if amount == 0:
+            break
+        num = amount // coin
+        amount -= num * coin
+        count += num
+    return count if amount == 0 else -1
